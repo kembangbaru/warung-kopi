b1ee3c6a6aa47a80055679849a589373c963efc2
unknown <kembangbaru40@gmail.com>
2024-02-29 12:32:36 +0700

    membuat website warung kopi

---
+++ kopi_kenangan/js/script.js
@@ -0,0 +1,13 @@
+// Toggle Active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// klik diluar hamburger menu dan side bar untuk keluar
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
